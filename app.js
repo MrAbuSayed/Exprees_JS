1ddb3f45ce41efbe1e6fe02c76b411f9f0969c53
Abu Sayed <mr.sayedf@yandex.com>
2023-08-20 13:51:15 +0600

    add route

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const express=require('express');
 const app=express();
 const userRoute= require('./route/user.route');
 
-app.use('/user/route',userRoute);
+app.use('/route',userRoute);
 
 app.get('/',(req,res)=>{
     res.send('Hellow Brow Whta`s up');
     res.end();
 });
+
+// html file add in server
+
+app.use('/home',(req,res)=>{
+    
+    res.sendFile(__dirname+"/files/index.html")
+});
+
+app.use('/register',(req,res)=>{
+    
+    res.sendFile(__dirname+"/files/register.html")
+    res.cookie("routeName","info");
+    res.cookie("patname","index");
+    res.cookie("status","nan");
+    // res.end();
+});
+
+
+
 // ERROR request
 app.use((req,res)=>{
  res.send("<h1>Error Madarsod</h1>");
  res.end();
 });
 
-
+                  
 
 module.exports=app;
--- route/user.route.js
+++ route/user.route.js
@@ -1,21 +1,60 @@
 const express= require('express');
 const route= express.Router();
 
-route.get('/about',(req,res)=>{
-    res.send('Hellow Brow Iam a Javascript Programmer');
-    res.end();
-});
-route.post('/about',(req,res)=>{
-    res.send('Hey broow Iam a programmer');
-    res.end();
-});
-route.put('/about',(req,res)=>{
-    res.send('Hey broow Iam a python programmer');
-    res.end();
-});
-route.delete('/about',(req,res)=>{
-    res.send('Hey broow Iam delete hoye gesi ');
-    res.end();
+// route.get('/about',(req,res)=>{
+//     res.send('Hellow Brow Iam a Javascript Programmer');
+//     res.end();
+// });
+// route.post('/about',(req,res)=>{
+//     res.send('Hey broow Iam a programmer');
+//     res.end();
+// });
+// route.put('/about',(req,res)=>{
+//     res.send('Hey broow Iam a python programmer');
+//     res.end();
+// });
+// route.delete('/about',(req,res)=>{
+//     res.send('Hey broow Iam delete hoye gesi ');
+//     res.end();
+// });
+
+
+// JSON format add in server
+route.get('/info',(req,res)=>{
+
+    res.status(201).json({
+        id:"S101",
+        name:"Abu Sayed",
+        work:"none"
+    });
+
+    // Dirctory changer
+    // res.redirect('/home');
+
+   res.end();
+})
+
+
+route.get('/login',(req,res)=>{
+  // Add coockies
+  res.cookie("routeName","login");
+  res.cookie("patname","none");
+  res.cookie("status","5858");
+
+  // Add Header
+    //  1st way
+ res.header("class","oneeleven");
+ res.header("pyhton","printF");
+ res.header("javascript","console.log");
+ 
+    //  2nd way
+
+//  res.append("class","oneeleven");
+//  res.append("pyhton","printF");
+//  res.append("javascript","console.log");
+
+ res.end();
+
 });
 
 
